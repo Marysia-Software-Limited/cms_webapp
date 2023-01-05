ac25d6dfe25106553c0456df526d7ca75c90fda2
monikamatis <monikamatis@gmail.com>
2023-01-05 01:28:38 +0000

    added configurations for urls for DjangoCMS

--- cms_webapp/urls.py
+++ cms_webapp/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Path below is required by DjangoCMS;
+    # It should come as last to allow for specific urls
+    # for other applications to be detected first.
+    re_path(r'^', include('cms.urls'))
 ]
